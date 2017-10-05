ed237bf0451631af1b34772d8ff06cb11ab6a8e1
Björn Martensen <bjoern.martensen@gmail.com>
2017-10-05 09:25:21 +0200

    debug

--- bootstrap.php
+++ bootstrap.php
@@ -1,12 +1,18 @@
 <?php
 
-die("dd");
+echo("before autoload");
 
 require_once 'vendor/autoload.php';
+
+echo( "after autoload" );
 
 $loader = new Twig_Loader_Filesystem( 'views' );
 $twig   = new Twig_Environment( $loader );
 
+echo( "got twig" );
+
 $manifest = json_decode( file_get_contents( __DIR__ .'/dist/manifest.json' ), true );
+
+echo( "got manifest" );
 
 $context['manifest'] = $manifest;
--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 
 require_once 'bootstrap.php';
 
+echo( "bootstrap complete" );
+
 $context['title'] = 'evm App';
 
 echo $twig->render( 'app.twig', $context );
