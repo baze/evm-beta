e22355d2bf1f972fbd84cae5614b28d9fbe15a52
Björn Martensen <bjoern.martensen@gmail.com>
2017-10-13 09:16:34 +0200

    stuff

--- index.php
+++ index.php
@@ -4,4 +4,6 @@ require_once 'bootstrap.php';
 
 $context['title'] = 'evm App';
 
+$context['displayType'] = isset( $_GET['display']) ? $_GET['display'] : null;
+
 echo $twig->render( 'app.twig', $context );
